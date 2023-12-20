b31057fdf53e0557ae1b30c905551f16b310a013
dongochoanthien <dongochoanthien@gmail.com>
2023-12-20 18:23:50 +0700

    rename logoBG

--- kien-linh/src/layout/landdingPage/introduction/index.tsx
+++ kien-linh/src/layout/landdingPage/introduction/index.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 import Image from "next/image";
-import logoBG from "@/assets/logo/logo-BG.png";
+import logoBG from "@/assets/logo/logo-bg.png";
 
 const Introduction = () => {
   return (
