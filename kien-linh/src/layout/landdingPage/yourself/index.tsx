40a5cf6e331a43d786078d2f0054be00e306cda8
dongochoanthien <dongochoanthien@gmail.com>
2023-12-25 20:00:06 +0700

    add yourself section

--- kien-linh/src/layout/landdingPage/yourself/index.tsx
+++ kien-linh/src/layout/landdingPage/yourself/index.tsx
@@ -1,7 +1,22 @@
 import React, { memo } from "react";
 
 const Yourself = () => {
-  return <section className="yourself">Yourself</section>;
+  return (
+    <section className="yourself">
+      <div className="yourself__groom" data-aos="zoom-in-left">
+        <div className="yourself__groom--image">Hình 02</div>
+        <div className="yourself__groom--name">Kiên</div>
+        <div className="yourself__groom--full-name">Lê Hữu Huỳnh Hiếu Kiên</div>
+        <div className="yourself__groom--line"></div>
+      </div>
+      <div className="yourself__bride" data-aos="zoom-in-left">
+        <div className="yourself__bride--image">Hình 03</div>
+        <div className="yourself__bride--name">Linh</div>
+        <div className="yourself__bride--full-name">Hoàng Vũ Huyền Linh</div>
+        <div className="yourself__groom--line"></div>
+      </div>
+    </section>
+  );
 };
 
 export default memo(Yourself);
