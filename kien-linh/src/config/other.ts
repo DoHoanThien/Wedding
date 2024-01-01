08a8ea110b26fdb944b136f670766b6ce8e0bbfc
dongochoanthien <dongochoanthien@gmail.com>
2024-01-01 18:35:14 +0700

    compress picture load in site

--- kien-linh/src/config/other.ts
+++ kien-linh/src/config/other.ts
@@ -1,58 +1,58 @@
 const ALBULM_LST = ["05", "06", "07","08","09", "10", "11", "12", "13", "14", "15", "16"]
 const ALBULM_LST_Demo = [
   {
-    src: "/static/assets/images/album/DSC_0353.png",
+    src: "/static/assets/images/album/DSC_0353.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_0672.png",
+    src: "/static/assets/images/album/DSC_0672.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_0740.png",
+    src: "/static/assets/images/album/DSC_0740.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_0902.png",
+    src: "/static/assets/images/album/DSC_0902.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_0940.png",
+    src: "/static/assets/images/album/DSC_0940.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_1041.png",
+    src: "/static/assets/images/album/DSC_1041.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_11011.png",
+    src: "/static/assets/images/album/DSC_11011.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/DSC_11012.png",
+    src: "/static/assets/images/album/DSC_11012.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/MRL03403.png",
+    src: "/static/assets/images/album/MRL03403.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/MRL03880.png",
+    src: "/static/assets/images/album/MRL03880.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/MRL04036.png",
+    src: "/static/assets/images/album/MRL04036.jpg",
     height: 250
   },  {
-    src: "/static/assets/images/album/MRL04279.png",
+    src: "/static/assets/images/album/MRL04279.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/MRL04390.png",
+    src: "/static/assets/images/album/MRL04390.jpg",
     height: 250
   },
   {
-    src: "/static/assets/images/album/MRL04407.png",
+    src: "/static/assets/images/album/MRL04407.jpg",
     height: 250
   }
 ]
