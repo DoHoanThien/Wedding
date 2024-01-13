8348341f3a17c8141250ed26322481a2b63d6270
dongochoanthien <dongochoanthien@gmail.com>
2024-01-13 21:47:08 +0700

    delete meta tag

--- kien-linh/src/pages/_app.tsx
+++ kien-linh/src/pages/_app.tsx
@@ -23,7 +23,6 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         ></meta>
-        <meta name="og:image" content="../../public/hinh4.jpg"></meta>
         <title>Thiệp Mời Hiếu Kiên - Huyền Linh</title>
       </Head>
       <Component {...pageProps} />
